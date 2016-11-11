3d45d97d19e3b37b38945f4c281bb9bccb25807d
moonrailgun <moonrailgun@gmail.com>
2016-11-11 19:07:25 +0800

    statisByActor

--- include/class/lvs/LVSStatis.class.php
+++ include/class/lvs/LVSStatis.class.php
@@ -1,11 +1,25 @@
 <?php
 class LVSStatis extends LVSBase{
-	private static $table_name = "statis";
+	private static $table_name = "item_log";
 
 	public static function getTabelName(){
 		return parent::$table_prefix.self::$table_name;
 	}
 
 	private static $columns = array('actorID','actorName','playerID','playerName','toolName','toolTypeName','totalCost','totalAmount','createdDate');
+
+	public static function statisByActor($actorID,$page,$pageSize){
+		$db=self::__instance();
+		$condition['AND']=array("actorID"=>$actorID);
+		$condition["ORDER"]=" createdDate desc";
+		$condition['LIMIT']=array($page,$pageSize);
+		
+		$list = $db->select(self::getTabelName(),self::$columns,$condition);
+		if($list){
+			var_dump($list);
+		}
+
+		return array ();
+	}
 }
 ?>
