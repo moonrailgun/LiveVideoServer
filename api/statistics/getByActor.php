37d35594551a943073761d566cf13faf6c7e4229
moonrailgun <moonrailgun@gmail.com>
2016-11-13 15:55:02 +0800

    方法迁移+代码清理

--- api/statistics/getByActor.php
+++ api/statistics/getByActor.php
@@ -5,10 +5,9 @@ $actorID = $sendStartTime = $sendEndTime = '';
 extract($_GET, EXTR_IF_EXISTS);
 
 if(Common::isGet()){
-	$list = LVSStatis::statisByActor($actorID,$sendStartTime,$sendEndTime);
+	$list = LVSStatis::statisByActorID($actorID,$sendStartTime,$sendEndTime);
 
 	//====整理数组
-	$is_checking_playerID = '';
 	$all_player_id_list = array();
 	$res = array();
 	//获取所有给该主播送道具的玩家名单
@@ -22,8 +21,8 @@ if(Common::isGet()){
 		$temp_array['actorID'] = $actorID;
 		$temp_array['actorName'] = $list[0]['actorName'];
 		$temp_array['playerID'] = $playerID;
-		$temp_array['playerName'] = LVSPlayer::getPlayerNameFromItemLogList($list,$playerID);
-		$data = LVSPlayer::getPlayerCostListFromItemLogList($list,$playerID);
+		$temp_array['playerName'] = LVSStatis::getPlayerNameFromItemLogList($list,$playerID);
+		$data = LVSStatis::getPlayerCostListFromItemLogList($list,$playerID);
 		$temp_array['totalCost'] = $data['totalCost'];
 		$temp_array['list'] = $data['list'];
 		array_push($res,$temp_array);
--- include/class/lvs/LVSStatis.class.php
+++ include/class/lvs/LVSStatis.class.php
@@ -8,7 +8,8 @@ class LVSStatis extends LVSBase{
 
 	private static $columns = array('actorID','actorName','playerID','playerName','toolName','toolTypeName','totalCost','totalAmount','createdDate');
 
-	public static function statisByActor($actorID,$sendStartTime,$sendEndTime){
+	//根据时间与主播ID获取统计信息
+	public static function statisByActorID($actorID,$sendStartTime,$sendEndTime){
 		$db=self::__instance();
 		$condition['AND']=array("actorID[=]"=>$actorID,"createdDate[<>]"=>[$sendStartTime,$sendEndTime]);
 		$condition['ORDER']=" createdDate desc";
@@ -20,6 +21,38 @@ class LVSStatis extends LVSBase{
 		}
 
 		return array ();
+	}
+
+	//getByActor
+	public static function getPlayerNameFromItemLogList($list,$playerID){
+		foreach ($list as $key => $value) {
+			if($value['playerID']==$playerID){
+				return $value['playerName'];
+			}
+		}
+		return '';
+	}
+
+	//getByActor
+	public static function getPlayerCostListFromItemLogList($list,$playerID){
+		$tmp_list = array();
+		$totalCost = 0;
+		foreach ($list as $key => $value) {
+			if($value['playerID'] == $playerID){
+				$totalCost += $value['totalCost'];
+
+				$tmp['toolName'] = $value['toolName'];
+				$tmp['toolTypeName'] = $value['toolTypeName'];
+				$tmp['totalCost'] = $value['totalCost'];
+				$tmp['totalAmount'] = $value['totalAmount'];
+				array_push($tmp_list, $tmp);
+			}
+		}
+		$res['totalCost'] = $totalCost;
+		$res['list'] = $tmp_list;
+		return $res;
+	}
+
 	}
 }
 ?>
