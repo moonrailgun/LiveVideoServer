4a14779b15d09467420181cabeb6dc427b602c04
moonrailgun <moonrailgun@gmail.com>
2016-11-13 15:39:07 +0800

    getByActor统计实现

--- api/statistics/getByActor.php
+++ api/statistics/getByActor.php
@@ -1,17 +1,33 @@
 <?php
-$data['actorID'] = 'lf001';
-$data['actorName'] = 'test';
-$data['playerID'] = 'lfplayer';
-$data['playerName'] = 'testplayer';
-$data['totalCost'] = '100';
+require ('../../include/init.inc.php');
 
-for($i=0;$i<2;$i++){
-	$item['toolTypeName'] = '233';
-	$item['toolName'] = '666';
-	$item['totalCost'] = '20';
-	$item['totalAmount'] = '5';
+$actorID = $sendStartTime = $sendEndTime = '';
+extract($_GET, EXTR_IF_EXISTS);
+
+if(Common::isGet()){
+	$list = LVSStatis::statisByActor($actorID,$sendStartTime,$sendEndTime);
+
+	//====整理数组
+	$is_checking_playerID = '';
+	$all_player_id_list = array();
+	$res = array();
+	//获取所有给该主播送道具的玩家名单
+	foreach ($list as $key => $item) {
+		array_push($all_player_id_list, $item['playerID']);
+	}
+	$all_player_id_list = array_unique($all_player_id_list);
+	//组合
+	foreach ($all_player_id_list as $key => $playerID) {
+		$temp_array = array();
+		$temp_array['actorID'] = $actorID;
+		$temp_array['actorName'] = $list[0]['actorName'];
+		$temp_array['playerID'] = $playerID;
+		$temp_array['playerName'] = LVSPlayer::getPlayerNameFromItemLogList($list,$playerID);
+		$data = LVSPlayer::getPlayerCostListFromItemLogList($list,$playerID);
+		$temp_array['totalCost'] = $data['totalCost'];
+		$temp_array['list'] = $data['list'];
+		array_push($res,$temp_array);
+	}
+	echo json_encode($res);
 }
-$data['list'][0] = $item;
-
-echo json_encode($data);
 ?>
