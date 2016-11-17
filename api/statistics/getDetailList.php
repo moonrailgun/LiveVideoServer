3ac35e4bd56b9290df99d350f2afae8aabc98d91
moonrailgun <moonrailgun@gmail.com>
2016-11-17 13:40:38 +0800

    getDetailList

--- api/statistics/getDetailList.php
+++ api/statistics/getDetailList.php
@@ -1,6 +1,25 @@
 <?php
 require ('../../include/init.inc.php');
 
-echo '[{"playerName":"","playerID":"","toolTypeName":"","toolName":"","sendTime":"","toolPerPrice":""}]';
+//echo '[{"playerName":"","playerID":"","toolTypeName":"","toolName":"","sendTime":"","toolPerPrice":""}]';
 
+$actorID = $sendStartTime = $sendEndTime = '';
+extract($_GET, EXTR_IF_EXISTS);
+
+if(Common::isGet()){
+	$list = LVSStatis::statisByActorID($actorID,$sendStartTime,$sendEndTime);
+
+	$res = array();
+	foreach ($list as $key => $value) {
+		$tmp = array();
+		$tmp['playerID'] = $value['playerID'];
+		$tmp['playerName'] = $value['playerName'];
+		$tmp['toolTypeName'] = $value['toolTypeName'];
+		$tmp['toolName'] = $value['toolName'];
+		$tmp['sendTime'] = $value['createdDate'];
+		$tmp['toolPerPrice'] = $value['totalCost'] / $value['totalAmount'];
+		$res[$key] = $tmp;
+	}
+	echo json_encode($res);
+}
 ?>
