e04f72c9f21da9f71bfa4c32238dae025554dcc3
moonrailgun <moonrailgun@gmail.com>
2016-12-13 08:50:42 +0800

    充值页面

--- pay/index.php
+++ pay/index.php
@@ -236,37 +236,39 @@
 		margin:0 auto;
 		text-align:center
 	}
+	.payment_method_frame{
+		background: url(./img/ico.png) right -106px #fff no-repeat;
+    border: 2px solid #0e9e3c;
+		display: inline-block;
+		padding: 6px 5px;
+		height: 18px;
+		line-height: 18px;
+		overflow: hidden;
+		border-radius: 3px;
+		position: relative;
+	}
+	.method_ico.Alipay{
+		width: 52px;
+		background-position: left -3px;
+		background: url(./img/payment_method.png) no-repeat;
+    display: inline-block;
+    height: 18px;
+    cursor: pointer;
+	}
 </style>
 <body>
 	<div class="header">
-		<div class="container black">
-			<div class="qrcode">
-				<div class="littlecode">
-					<img width="16px" src="img/little_qrcode.jpg" id="licode">
-					<div class="showqrs" id="showqrs">
-						<div class="shtoparrow"></div>
-						<div class="guanzhuqr">
-							<img src="img/guanzhu_qrcode.png" width="80">
-							<div class="shmsg" style="margin-top:5px;">
-							请扫码关注
-							</div>
-							<div class="shmsg" style="margin-bottom:5px;">
-								接收重要信息
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 		<div class="container">
 			<div class="nav">
 				<a href="https://www.alipay.com/" class="logo"><img src="img/alipay_logo.png" height="30px"></a>
 				<span class="divier"></span>
 				<a href="http://open.alipay.com/platform/home.htm" class="open" target="_blank">开放平台</a>
-				<ul class="navbar">
+				<span class="divier"></span>
+				<a href="../" class="open" style="width:120px;" target="_blank">回到管理平台</a>
+				<!-- <ul class="navbar">
 					<li><a href="https://doc.open.alipay.com/doc2/detail?treeId=62&articleId=103566&docType=1" target="_blank">在线文档</a></li>
 					<li><a href="https://cschannel.alipay.com/portal.htm?sourceId=213" target="_blank">技术支持</a></li>
-				</ul>
+				</ul> -->
 			</div>
 		</div>
 		<div class="container blue">
@@ -276,32 +278,51 @@
 	<div class="content">
 		<form action="alipayapi.php" class="alipayform" method="post" target="_blank">
 			<div class="element" style="margin-top:60px;">
-				<div class="legend">支付宝即时到账交易接口快速通道 </div>
+				<div class="legend">快速充值</div>
 			</div>
-			<div class="element">
-				<div class="etitle">商户订单号:</div>
-				<div class="einput"><input type="text" name="WIDout_trade_no" id="out_trade_no"></div>
-				<br>
-				<div class="mark">注意：商户订单号(out_trade_no).必填(建议是英文字母和数字,不能含有特殊字符)</div>
-			</div>
+			<!-- <div class="element"> -->
+				<!-- <div class="etitle">商户订单号:</div> -->
+				<!-- <div class="einput"> -->
+					<input type="hidden" name="WIDout_trade_no" id="out_trade_no">
+				<!-- </div> -->
+				<!-- <br> -->
+				<!-- <div class="mark">注意：商户订单号(out_trade_no).必填(建议是英文字母和数字,不能含有特殊字符)</div> -->
+			<!-- </div> -->
 
+			<!-- <div class="element"> -->
+				<!-- <div class="etitle">商品名称:</div> -->
+				<!-- <div class="einput"> -->
+					<input type="hidden" name="WIDsubject" value="视频直播充值">
+				<!-- </div> -->
+				<!-- <br> -->
+				<!-- <div class="mark">注意：产品名称(subject)，必填(建议中文，英文，数字，不能含有特殊字符)</div> -->
+			<!-- </div> -->
 			<div class="element">
-				<div class="etitle">商品名称:</div>
-				<div class="einput"><input type="text" name="WIDsubject" value="test商品123"></div>
-				<br>
-				<div class="mark">注意：产品名称(subject)，必填(建议中文，英文，数字，不能含有特殊字符)</div>
+				<div class="etitle">充值账户:</div>
+				<div class="einput"><input type="text" name="userID" value="moonrailgun"></div>
+				<!-- <br> -->
+				<!-- <div class="mark">注意：产品名称(subject)，必填(建议中文，英文，数字，不能含有特殊字符)</div> -->
 			</div>
 			<div class="element">
-				<div class="etitle">付款金额:</div>
+				<div class="etitle">充值金额:</div>
 				<div class="einput"><input type="text" name="WIDtotal_fee" value="0.01"></div>
-				<br>
-				<div class="mark">注意：付款金额(total_fee)，必填(格式如：1.00,请精确到分)</div>
+				<!-- <br> -->
+				<!-- <div class="mark">注意：付款金额(total_fee)，必填(格式如：1.00,请精确到分)</div> -->
 			</div>
 			<div class="element">
-				<div class="etitle">商品描述:</div>
-				<div class="einput"><input type="text" name="WIDbody" value="即时到账测试"></div>
+				<div class="etitle">充值方式:</div>
+				<div class="einput">
+					<a href="javascript:void(0);" for="payment_method" class="payment_method_frame" value="19">
+						<label class="method_ico Alipay"></label>
+					</a>
+				</div>
 				<br>
-				<div class="mark">注意：商品描述(body)，选填(建议中文，英文，数字，不能含有特殊字符)</div>
+			</div>
+			<div class="element">
+				<!-- <div class="etitle">商品描述:</div> -->
+				<div class="einput"><input type="hidden" name="WIDbody" value="即时到账测试"></div>
+				<br>
+				<!-- <div class="mark">注意：商品描述(body)，选填(建议中文，英文，数字，不能含有特殊字符)</div> -->
 			</div>
 			<div class="element">
 				<input type="submit" class="alisubmit" value ="确认支付">
@@ -326,14 +347,14 @@
 	</div>
 </body>
 <script>
-	var even = document.getElementById("licode");
-	var showqrs = document.getElementById("showqrs");
-	 even.onmouseover = function(){
-	 	showqrs.style.display = "block";
-	 }
-	 even.onmouseleave = function(){
-	 	showqrs.style.display = "none";
-	 }
+	// var even = document.getElementById("licode");
+	// var showqrs = document.getElementById("showqrs");
+	//  even.onmouseover = function(){
+	//  	showqrs.style.display = "block";
+	//  }
+	//  even.onmouseleave = function(){
+	//  	showqrs.style.display = "none";
+	//  }
 
 	 var out_trade_no = document.getElementById("out_trade_no");
 
@@ -356,7 +377,7 @@
            return format;
        };
 
-	 out_trade_no.value = 'test'+ new Date().format("yyyyMMddhhmmss");
+	 out_trade_no.value = 'lvs'+ new Date().format("yyyyMMddhhmmss");
 </script>
 
 </html>
