bf24aba0f8a4d0fd2444e35c11b383283fa435c6
moonrailgun <moonrailgun@gmail.com>
2016-12-30 13:37:02 +0800

    规则类多种查询方式

--- include/class/lvs/LVSRule.class.php
+++ include/class/lvs/LVSRule.class.php
@@ -40,5 +40,39 @@ class LVSRule extends LVSBase{
 		$data = json_decode('{"deviceID":"sn1001","state":1,"stateDescription":"正常","toolDirective":[{"toolName":"头套1","state":1,"stateDescription":"正常"}]}');
 		return $data;
 	}
+
+	public static function getRuleByName($rule_name){
+		$db = self::__instance();
+		$condition = array("ruleName[=]"=>$ruleName);
+		$list = $db->select(self::getTableName(),self::$columns,$condition);
+		if ($list) {
+			return $list[0];
+		}
+		return array ();
+	}
+
+	public static function getAcquisitionRule(){
+		return self::getRuleByName("acquisitionRule");
+	}
+
+	public static function getControllerDirectiveRule(){
+		return self::getRuleByName("controllerDirectiveRule");
+	}
+
+	public static function getCostControlRule(){
+		return self::getRuleByName("costControlRule");
+	}
+
+	public static function getGift2SenderInfoRule(){
+		return self::getRuleByName("gift2SenderInfoRule");
+	}
+
+	public static function getTool2DirectiveRule(){
+		return self::getRuleByName("tool2DirectiveRule");
+	}
+
+	public static function getCommonRule(){
+		return self::getRuleByName("commonRule");
+	}
 }
 ?>
